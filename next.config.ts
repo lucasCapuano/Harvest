9c850b575c942b1eed3683ab2e95350a332b4aa4
Lucas Capuano <capuanolucas@gmail.com>
2026-04-03 11:12:57 +0200

    feat: redirect / to /applications as default home page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/applications",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
